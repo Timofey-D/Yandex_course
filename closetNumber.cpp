67a45167bcca5d7f9c07c43e5b0037770a4fb0b3
TimofeyD <Timofey.Dankevich@gmail.com>
2020-12-04 15:13:08 +0200

    White

---
+++ closetNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 6;
+	int b = 4;
+	int c = 1;
+	cout << a / b << endl;
+	cout << 1 / 0.4 << endl;
+	cout << a / b + c / 0.4 << endl;
+	return 0;
+}
